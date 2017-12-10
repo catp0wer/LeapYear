92f269b0753e13b3faef9873bf484bfecf714569
laila <laila@fake.com>
2017-12-10 21:55:08 +0200

    adding functionality to read from keyboard the number of leap years to be displayed, and the functionality which find leap years

--- src/com/company/LeapYear.java
+++ src/com/company/LeapYear.java
@@ -1,17 +1,40 @@
 package com.company;
 
-/**
- * Created by Liuda on 12/10/2017.
- */
+import java.util.ArrayList;
+import java.util.Calendar;
+
 public class LeapYear {
 
-    public boolean isLeap(int year){
+    /**
+     * @param year -> the year to be checked if it is leap or not
+     * @return -> boolean (if the year is leap or not)
+     */
+    private boolean isLeap(int year){
         if(year%4 ==0 || year%100==0 && year%400 == 0){
-            System.out.println("Leap year");
            return true;
 
         }else
-            System.out.println("Not a leap year");
             return false;
+    }
+
+    /**
+     * @param number - > number of next years to be displayed(from keyboard)
+     * @return -> an ArrayList which includes the leap years
+     */
+    public ArrayList findLeapYears(int number){
+        ArrayList<Integer> leapYears = new ArrayList<Integer>();
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        System.out.println("Current year is "+year);
+        while(true){
+            if(isLeap(year)){
+                leapYears.add(year);
+            }
+            if(leapYears.size() == number){
+                return leapYears;
+            } else{
+                year++;
+            }
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args){
+        System.out.println("Enter the number of next leap years to be displayed: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
         LeapYear year = new LeapYear();
-        year.isLeap(2016);
+        ArrayList result = year.findLeapYears(number);
+        System.out.println(result);
     }
 
 
